103d3ecf3b8ca12c264843026706ec40d7dadecc
Leon-Arno <leon.rimrod@gmail.com>
2019-08-06 11:21:44 +0200

    Added Solution

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let button = document.querySelector('button');
+let list = document.querySelector('ul');
+
+let listVisible = false;
+let paragraphIsShown = true;
+
+button.addEventListener('click', function() {
+  listVisible = !listVisible;
+  if (listVisible) {
+    list.style.display = 'block';
+    button.innerHTML = 'Get them out of here!';
+  } else {
+    list.style.display = 'none';
+    button.innerHTML = 'Show favorite destinations';
+  }
+});
